80d3c2da83b6d4ba1c9cdc420d4c7c9dd21b3afb
Michael Liu <84533632+michaelmml@users.noreply.github.com>
2023-08-27 16:42:43 +0100

    Update planetary-demo.py

--- planetary-demo.py
+++ planetary-demo.py
@@ -246,7 +246,7 @@ def schrodinger():
         R, Theta = np.meshgrid(r, theta)
         
         # States to consider in the 3x3 grid
-        states = [(1, 0, 0), (2, 0, 0), (2, 1, 0),
+        states = [(4, 0, 0), (4, 2, 0), (4, 2, 1),
                   (2, 1, 1), (3, 0, 0), (3, 1, 0),
                   (3, 1, 1), (3, 2, 0), (3, 2, 1)]
         
